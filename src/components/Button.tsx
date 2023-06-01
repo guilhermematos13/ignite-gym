5e7baa4ea38a679e0ce07216b5ed7c27a2c13787
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-01 15:05:44 -0300

    feat: creating button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base';
+
+type Props = IButtonProps & {
+  title: string;
+};
+
+export function Button({ title, variant, ...rest }: Props) {
+  return (
+    <ButtonNativeBase
+      w="full"
+      h={14}
+      bg={variant === 'outline' ? 'transparent' : 'green.700'}
+      borderWidth={variant === 'outline' ? 1 : 0}
+      borderColor="green.500"
+      _pressed={{ bg: variant === 'outline' ? 'gray.500' : 'green.500' }}
+      {...rest}
+    >
+      <Text
+        color={variant === 'outline' ? 'green.500' : 'white'}
+        fontFamily="body"
+        fontSize="sm"
+        rounded="sm"
+      >
+        {title}
+      </Text>
+    </ButtonNativeBase>
+  );
+}
