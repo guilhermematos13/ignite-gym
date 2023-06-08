ca41ed37bf7bf17924f5a3786c3040ae3fe88764
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-06-08 14:11:07 -0300

    fix: adjusting code

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -29,7 +29,9 @@ export function Home() {
           renderItem={({ item }) => (
             <Group
               name={item}
-              isActive={groupSelected === item}
+              isActive={
+                groupSelected.toLocaleLowerCase() === item.toLocaleLowerCase()
+              }
               onPress={() => setGroupSelected(item)}
             />
           )}
